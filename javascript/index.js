e8496d443a971df19a99f49ed169cf59aee13b5e
bayudsatriyo <bayudsatriyo@gmail.com>
2023-12-19 04:39:06 +0700

    first commit

---
+++ javascript/index.js
@@ -0,0 +1,39 @@
+const DrawerInitiator = {
+    init({ button, drawer, content, drawerhome, buttonhome }) {
+      button.addEventListener('click', (event) => {
+        this._toggleDrawer(event, drawer);
+      });
+  
+      buttonhome.addEventListener('click', (event) => {
+        this._toggleDrawerhome(event, drawerhome);
+      });
+  
+      content.addEventListener('click', (event) => {
+        this._closeDrawer(event, drawer);
+        this._closeDrawerhome(event, drawerhome);
+      });
+    },
+  
+    _toggleDrawer(event, drawer) {
+      event.stopPropagation();
+      drawer.classList.toggle('open');
+    },
+  
+    _toggleDrawerhome(event, drawerhome) {
+      event.stopPropagation();
+      drawerhome.classList.toggle('active');
+    },
+  
+    _closeDrawer(event, drawer) {
+      event.stopPropagation();
+      drawer.classList.remove('open');
+    },
+  
+    _closeDrawerhome(event, drawerhome) {
+      event.stopPropagation();
+      drawerhome.classList.remove('active');
+    },
+  };
+
+const button = document.getElementById('#hamburgerButtonHome');
+const content = document.getElementById()
